3eb667da0d20d680673927395e6147fa756d5202
Dr-Click <mostafa.ragab@espace.com.eg>
2017-10-19 14:40:05 +0200

    add enquiry model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :enquiries, only: [:new, :show, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # mount weather_api if it has routes
   # mount WeatherApi::Engine => "/weather_api"
+  
+  root :to => "enquiries#new"
 end
